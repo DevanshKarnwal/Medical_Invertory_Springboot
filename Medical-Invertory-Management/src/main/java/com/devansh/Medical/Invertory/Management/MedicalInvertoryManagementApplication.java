2d622d5f0506bec63189da57658a28b27e2dad0f
Devansh Karnwal <devansh10.karnwal@gmail.com>
2025-07-24 14:23:42 +0530

    project started

---
+++ Medical-Invertory-Management/src/main/java/com/devansh/Medical/Invertory/Management/MedicalInvertoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.devansh.Medical.Invertory.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalInvertoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalInvertoryManagementApplication.class, args);
+	}
+
+}
